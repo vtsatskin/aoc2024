8d3f81350e745b3ce2037a74e098c05bb0ee8616
Valentin Tsatskin <val@valtsatskin.com>
2024-12-01 16:50:28 -0500

    day 1 part 2

--- day1.py
+++ day1.py
@@ -22,11 +22,31 @@ def sum_diff(left: List[int], right: List[int]) -> int:
         sum += abs(x - y)
     return sum
 
+def frequency(arr: List[int]) -> dict[int, int]:
+    freq = {}
+    for x in arr:
+        freq[x] = freq.get(x, 0) + 1
+    return freq
+
+def similarity_score(left: List[int], right: List[int]) -> int:
+    right_freq = frequency(right)
+
+    sum = 0
+    for x in left:
+        sum += x * right_freq.get(x, 0)
+    return sum
+    
 
 if __name__ == '__main__':
     [left, right] = parse_input("inputs/day1.txt")
+
+    # Part 1:
     left_sorted = sorted(left)
     right_sorted = sorted(right)
 
     sum_diffs = sum_diff(left_sorted, right_sorted)
     print("Sum of differences:", sum_diffs)
+
+    # Part 2:
+    similarity = similarity_score(left, right)
+    print("Similarity score:", similarity)
