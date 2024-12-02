cae37ec8f80ec3245b57d445edb33cc999a04282
Valentin Tsatskin <val@valtsatskin.com>
2024-12-02 12:37:52 -0500

    day 2 part 2

--- day2.py
+++ day2.py
@@ -14,24 +14,30 @@ def parse_input(file_path, split_char='\n') -> List[List[int]]:
             reports.append(report)
     return reports
 
-def is_report_safe(report: List[int]) -> bool:
+def is_report_safe(report: List[int], dampen_problem: bool = False) -> bool:
     assert len(report) >= 2
 
     increasing = report[1] > report[0]
     for prev, cur in zip(report, report[1:]):
         diff = cur - prev
-        if increasing and not (diff >= 1 and diff <= 3):
+        problem = (increasing and not (diff >= 1 and diff <= 3)) or ((not increasing) and not (diff <= -1 and diff >= -3))
+        if problem and not dampen_problem:
             return False
-        elif (not increasing) and not (diff <= -1 and diff >= -3):
-            return False
+        elif problem and dampen_problem:
+            return any(is_report_safe(report[:i] + report[i+1:]) for i in range(len(report)))
 
     return True
 
 def count_safe_reports(reports: List[List[int]]) -> int:
     return sum([1 if is_report_safe(report) else 0 for report in reports])
 
+def count_safe_reports_dampened(reports: List[List[int]]) -> int:
+    return sum([1 if is_report_safe(report, True) else 0 for report in reports])
+
 if __name__ == '__main__':
     reports = parse_input("./inputs/day2.txt")
     safe_reports = count_safe_reports(reports)
+    safe_reports_dampened = count_safe_reports_dampened(reports)
 
     print("Number of safe reports:", safe_reports)
+    print("Number of safe reports (dampened):", safe_reports_dampened)
